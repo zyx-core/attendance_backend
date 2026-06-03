69ece25eaa18c134a4ee6854f16bb03d01490918
arjunhemanth6@gmail.com <arjun.c@nestgroup.net>
2026-06-04 01:00:22 +0530

    Added attendance migration

--- studentBackend/obj/Debug/net8.0/studentBackend.AssemblyInfo.cs
+++ studentBackend/obj/Debug/net8.0/studentBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("studentBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259be1d9eab0dbf00fb518f5a048d0f083e2a033")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9fb5fba4bc7b23f7dc64b63f57e353342e9612e")]
 [assembly: System.Reflection.AssemblyProductAttribute("studentBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("studentBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
